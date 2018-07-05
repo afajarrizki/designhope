a3dfb2980686d13d6a76af98477174630c373d89
afajarrizki <40469587+afajarrizki@users.noreply.github.com>
2018-07-05 22:08:32 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="jquery-3.1.1.min.js"></script>
+	<title></title>
+</head>
+<body>
+	<script type="text/javascript">
+		function tampilkan(){
+			$(".headerright ul li a").click(function(){
+				$(".headerleft img").hide();
+					});
+		}
+	</script>
+</body>
+</html>
